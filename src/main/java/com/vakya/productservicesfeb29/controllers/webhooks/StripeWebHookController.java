9a8516cfe9020768049ff5e1aea096c1c5778df9
Kankipati Vakdevi <vakdevikankipati@gmail.com>
2024-04-08 12:15:29 +0530

    changes

--- src/main/java/com/vakya/productservicesfeb29/controllers/webhooks/StripeWebHookController.java
+++ src/main/java/com/vakya/productservicesfeb29/controllers/webhooks/StripeWebHookController.java
@@ -2,13 +2,14 @@ package com.vakya.productservicesfeb29.controllers.webhooks;
 
 import com.stripe.net.Webhook;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/webhooks/stripe")
 public class StripeWebHookController {
-    @GetMapping("/")
+    @PostMapping("/")
     public void handleWebhookRequest(Webhook webhook){
 
     }
